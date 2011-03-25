3d5c9fce29f1c395bbdd52759a01121b67872fda
David Sauve <david.sauve@bag-of-holding.com>
2011-03-25 10:39:50 -0700

    Refactored tools to use appropriate command type.  Mostly TextCommand, with one WindowCommand.  add/commit/rm/reset/status/diff all working now.

--- GitCommand.py
+++ GitCommand.py
@@ -5,74 +5,70 @@ import sublime
 import sublime_plugin
 
 
-class GitCommandBase(sublime_plugin.WindowCommand):
-    def __init__(self, *args, **kwargs):
-        super(GitCommandBase, self).__init__(*args, **kwargs)
+def exec_command(command_string):
+    p = subprocess.Popen(command_string, shell=True, bufsize=1024, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p.wait()
 
-        self.folder_name, self.file_name = os.path.split(self.window.active_view().file_name())
+    stdout = p.stdout
+    stderr = p.stderr
 
-        os.chdir(self.folder_name)
-
-    def exec_command(self, command_string):
-        p = subprocess.Popen(command_string, shell=True, bufsize=1024, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        p.wait()
-
-        stdout = p.stdout
-        stderr = p.stderr
-
-        return '\n'.join([stdout.read(), stderr.read()])
-
-    def show_results(self, results):
-        self.output_view = self.window.get_output_panel('git')
-        self.window.get_output_panel('git')
-        self.window.run_command('show_panel', {'panel': 'output.git'})
-        self.output_view.set_read_only(False)
-        
-        edit = self.output_view.begin_edit()
-
-        self.output_view.insert(edit, self.output_view.size(), results)
-        self.output_view.end_edit(edit)
-        self.output_view.set_read_only(True)
+    return '\n'.join([stdout.read(), stderr.read()])
 
 
-class GitStatusCommand(GitCommandBase):
-    def run(self):
-        self.show_results(self.exec_command('git status'))
+def show_results(window, results):
+    output_view = window.get_output_panel('git')
+    window.get_output_panel('git')
+    window.run_command('show_panel', {'panel': 'output.git'})
+    output_view.set_read_only(False)
+
+    edit = output_view.begin_edit()
+
+    output_view.insert(edit, output_view.size(), results)
+    output_view.end_edit(edit)
+    output_view.set_read_only(True)
 
 
-class GitDiffCommand(GitCommandBase):
-    def run(self):
-        self.show_results(self.exec_command('git diff'))
+class GitStatusCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        os.chdir(os.path.dirname(self.view.file_name()))
+        show_results(self.view.window(), exec_command('git status'))
 
 
-class GitCommitWithMessageCommand(GitCommandBase):
-    def run(self, message):
-        # TODO: Why doesn't this show the output?
-        self.show_results(self.exec_command('git commit -m "%s"' % message.replace('"', '\"')))
+class GitDiffCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        os.chdir(os.path.dirname(self.view.file_name()))
+        show_results(self.view.window(), exec_command('git diff'))
 
 
-class GitCommitCommand(GitCommandBase):
+class GitAddCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        cmd = 'git add %s' % self.view.file_name()
+        os.chdir(os.path.dirname(self.view.file_name()))
+        exec_command(cmd)
+        show_results(self.view.window(), cmd)
+
+
+class GitResetCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        cmd = 'git reset HEAD %s' % self.view.file_name()
+        os.chdir(os.path.dirname(self.view.file_name()))
+        exec_command(cmd)
+        show_results(self.view.window(), cmd)
+
+
+class GitCommitWithMessageCommand(sublime_plugin.TextCommand):
+    def run(self, edit, message):
+        os.chdir(os.path.dirname(self.view.file_name()))
+        show_results(self.view.window(), exec_command('git commit -m "%s"' % message.replace('"', '\"')))
+
+
+class GitCommitCommand(sublime_plugin.WindowCommand):
     def on_done(self, text):
         try:
             if self.window.active_view():
-                self.window.run_command('git_commit_with_message', {'message': text})
+                self.window.active_view().run_command('git_commit_with_message', {'message': text})
         except ValueError:
             pass
     
     def run(self):
         self.window.show_input_panel("Commit Message:", "", self.on_done, None, None)
-
-
-class GitAddCommand(GitCommandBase):
-    def run(self):
-        self.exec_command('git add %s' % self.file_name)
-        self.show_results('Added %s' % self.file_name)
-
-# class GitInitCommand(GitCommandBase):
-#    def run(self):
-#        TODO: This should probably prompt for where to `init` rather than assuming the same folder as `file_name`
-#        self.show_results(self.exec_command('git init'))
-
-class GitRmCommand(GitCommandBase):
-    def run(self):
-        self.show_results(self.exec_command('git rm %s' % self.file_name))
