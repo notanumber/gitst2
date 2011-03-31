521117f15d7c53549be071fcc7ed3a59f8e3e668
David Sauve <david.sauve@bag-of-holding.com>
2011-03-31 13:43:43 -0700

    Yet another big refactor.  Removed all WindowCommands as it didn't make sense to have them when all command require at least a folder to operate on

--- GitCommand.py
+++ GitCommand.py
@@ -5,64 +5,64 @@ import sublime
 import sublime_plugin
 
 
-def exec_command(command_string):
-    p = subprocess.Popen(command_string, shell=True, bufsize=1024, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    p.wait()
-
-    stdout = p.stdout
-    stderr = p.stderr
-
-    return '\n'.join([stdout.read(), stderr.read()])
-
-
 class GitTextCommandBase(sublime_plugin.TextCommand):
-    def is_enabled(self, *args):
-        file_name = self.view.file_name()
-        if file_name:
-            os.chdir(os.path.dirname(file_name))
-            return os.system('git rev-parse 2> /dev/null > /dev/null') == 0
-        else:
-            return False
+    folder_name = ''
+    file_name = ''
 
+    def __init__(self, *args, **kwargs):
+        super(GitTextCommandBase, self).__init__(*args, **kwargs)
 
-class GitWindowCommandBase(sublime_plugin.WindowCommand):
+        if self.view.file_name():
+            self.folder_name, self.file_name = os.path.split(self.view.file_name())
+  
+    def exec_command(self, command_string, cwd=None):
+        if not cwd:
+            cwd = self.folder_name
+        p = subprocess.Popen(
+            command_string, cwd=cwd, shell=True, bufsize=1024,
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        p.wait()
+
+        stdout = p.stdout
+        stderr = p.stderr
+
+        return '\n'.join([stdout.read(), stderr.read()])
+    
+    def show_output(self, output):
+        window = self.view.window()
+        view = window.get_output_panel('git')
+        edit = view.begin_edit()
+
+        view.set_read_only(False)
+        view.insert(edit, view.size(), output)
+        view.end_edit(edit)
+        view.set_read_only(True)
+
+        window.run_command('show_panel', {'panel': 'output.git'})
+
     def is_enabled(self, *args):
-        file_name = self.window.active_view().file_name()
-        if file_name:
-            os.chdir(os.path.dirname(file_name))
-            return os.system('git rev-parse 2> /dev/null > /dev/null') == 0
+        if self.folder_name:
+            p = subprocess.Popen('git rev-parse 2> /dev/null > /dev/null', cwd=self.folder_name, shell=True)
+            return p.wait() == 0
         else:
             return False
 
 
 class GitStatusCommand(GitTextCommandBase):
     def run(self, edit):
-        os.chdir(os.path.dirname(self.view.file_name()))
-
-        results = exec_command('git status')
-        window = self.view.window()
-        view = window.get_output_panel('git')
-        edit = view.begin_edit()
-
-        view.set_read_only(False)
-        view.insert(edit, view.size(), results)
-        view.end_edit(edit)
-        view.set_read_only(True)
-
-        window.run_command('show_panel', {'panel': 'output.git'})
+        self.show_output(self.exec_command('git status'))
 
 
 class GitDiffCommand(GitTextCommandBase):
     def run(self, edit):
-        os.chdir(os.path.dirname(self.view.file_name()))
-
-        results = exec_command('git diff')
+        results = self.exec_command('git diff')
         window = self.view.window()
         view = self.view.window().new_file()
         edit = view.begin_edit()
 
         view.set_scratch(True)
-        view.set_name('%s.diff' % os.path.basename(self.view.file_name()))
+        view.set_name('%s.diff' % os.path.basename(self.file_name))
         view.set_syntax_file('Packages/Diff/Diff.tmLanguage')
 
         view.insert(edit, 0, results)
@@ -71,95 +71,75 @@ class GitDiffCommand(GitTextCommandBase):
 
 class GitAddCommand(GitTextCommandBase):
     def run(self, edit):
-        os.chdir(os.path.dirname(self.view.file_name()))
-        print exec_command('git add %s' % self.view.file_name())
+        self.exec_command('git add %s' % self.file_name)
+        self.show_output('Added %s' % self.file_name)
 
 
 class GitRmCommand(GitTextCommandBase):
     def run(self, edit):
-        os.chdir(os.path.dirname(self.view.file_name()))
-        print exec_command('git rm %s' % self.view.file_name())
+        self.show_output(self.exec_command('git rm %s' % self.file_name))
 
 
 class GitResetCommand(GitTextCommandBase):
     def run(self, edit):
-        os.chdir(os.path.dirname(self.view.file_name()))
-        print exec_command('git reset HEAD %s' % self.view.file_name())
+        self.show_output(self.exec_command('git reset HEAD %s' % self.file_name))
 
 
 class GitLogCommand(GitTextCommandBase):
     def run(self, edit):
-        os.chdir(os.path.dirname(self.view.file_name()))
-
-        results = exec_command('git log')
-        window = self.view.window()
-        view = window.get_output_panel('git')
-        edit = view.begin_edit()
-
-        view.set_read_only(False)
-        view.insert(edit, view.size(), results)
-        view.end_edit(edit)
-        view.set_read_only(True)
-
-        window.run_command('show_panel', {'panel': 'output.git'})
+        self.show_output(self.exec_command('git log'))
 
 
 class GitCommitWithMessageCommand(GitTextCommandBase):
     def run(self, edit, message):
-        os.chdir(os.path.dirname(self.view.file_name()))
-        print exec_command('git commit -m "%s"' % message.replace('"', '\"'))
+        self.show_output(self.exec_command('git commit -m "%s"' % message.replace('"', '\"')))
 
 
-class GitCommitCommand(GitWindowCommandBase):
+class GitCommitCommand(GitTextCommandBase):
     def on_done(self, text):
         try:
-            if self.window.active_view():
-                self.window.active_view().run_command('git_commit_with_message', {'message': text})
+            if self.view:
+                self.view.run_command('git_commit_with_message', {'message': text})
         except ValueError:
             pass
     
-    def run(self):
-        self.window.show_input_panel('Commit Message:', '', self.on_done, None, None)
+    def run(self, edit):
+        self.view.window().show_input_panel('Commit Message:', '', self.on_done, None, None)
 
 
 class GitTagWithNameCommand(GitTextCommandBase):
     def run(self, edit, tag):
-        os.chdir(os.path.dirname(self.view.file_name()))
-        print exec_command('git tag %s' % tag)
+        self.show_output(self.exec_command('git tag %s' % tag))
 
 
-class GitTagCommand(GitWindowCommandBase):
+class GitTagCommand(GitTextCommandBase):
     def on_done(self, text):
         try:
-            if self.window.active_view():
-                self.window.active_view().run_command('git_tag_with_name', {'tag': text})
+            if self.view:
+                self.view.run_command('git_tag_with_name', {'tag': text})
         except ValueError:
             pass
     
-    def run(self):
-        self.window.show_input_panel('Tag Name:', '', self.on_done, None, None)
+    def run(self, edit):
+        self.view.window().show_input_panel('Tag Name:', '', self.on_done, None, None)
 
 
 class GitInitInFolderCommand(GitTextCommandBase):
-    def is_enabled(self, *args):
-        return not super(GitInitInFolderCommand, self).is_enabled(*args)
-
     def run(self, edit, folder):
-        os.chdir(folder)
-        print exec_command('git init')
+        self.show_output(self.exec_command('git init', cwd=folder))
 
 
-class GitInitCommand(GitWindowCommandBase):
+class GitInitCommand(GitTextCommandBase):
     def is_enabled(self, *args):
         return not super(GitInitCommand, self).is_enabled(*args)
 
     def on_done(self, text):
         try:
-            if self.window.active_view():
-                self.window.active_view().run_command('git_init_in_folder', {'folder': text})
+            if self.view:
+                self.view.run_command('git_init_in_folder', {'folder': text})
+
         except ValueError:
             pass
     
-    def run(self):
-        folder_name = os.path.dirname(self.window.active_view().file_name())
-        self.window.show_input_panel('Project Folder:', folder_name, self.on_done, None, None)
+    def run(self, edit):
+        self.view.window().show_input_panel('Project Folder:', self.folder_name, self.on_done, None, None)
